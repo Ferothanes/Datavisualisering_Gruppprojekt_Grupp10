96b3ad278d2fd68f87a4531a08bb79f16bce8033
Freja Ferothanes <freja_kind@hotmail.com>
2025-05-20 10:52:59 +0200

    commit

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ import taipy.gui.builder as tgb
 from taipy.gui import Gui
 from frontend.pages.Overview import gender_age
 from frontend.pages.utbildningsomrade import utbildningsomrade
+from frontend.pages.statistikansökningar import ansökningar
 
 #pages = {"home": home_page, "dashboard": dashboard_page, "data": data_page}
-pages = {"Overview": gender_age, "Utbildningsområde": utbildningsomrade}
+pages = {"Overview": gender_age, "Utbildningsområde": utbildningsomrade, "statistikansökningar": ansökningar,}
 
 
 if __name__ == "__main__":
     Gui(pages=pages, css_file="assets/main.css").run(dark_mode=False, use_reloader=True, port=8080) #pages = pages 
+
 
 
