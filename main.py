6425d898c3c5120babf11a4bfd2b342ca779b6b1
Ferothanes <freja_kind@hotmail.com>
2025-05-22 13:12:38 +0200

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from frontend.pages.home import home_page
 from frontend.pages.anordnare import anordnare
 
 #pages = {"home": home_page, "dashboard": dashboard_page, "data": data_page}
-pages = {"Home": home_page, "Kursansokningar": ansökningar, "Utbildningsomraden": utbildningsomrade, "Demografi": gender_age, "Anordnare":anordnare}
+pages = {"Home": home_page, "Anordnare":anordnare, "Kursansokningar": ansökningar, "Utbildningsomraden": utbildningsomrade, "Demografi": gender_age}
 
 
 if __name__ == "__main__":
