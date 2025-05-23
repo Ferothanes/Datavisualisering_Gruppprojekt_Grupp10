4c8bcc2e54fdfa912df173fdbcd00de7e92bbbc9
Ferothanes <freja_kind@hotmail.com>
2025-05-23 09:12:29 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 import taipy.gui.builder as tgb
 from taipy.gui import Gui
+
 from frontend.pages.utbildningsomrade import utbildningsomrade
 from frontend.pages.statistikansökningar import ansökningar
 from frontend.pages.Overview import gender_age
 from frontend.pages.home import home_page
+from frontend.pages.anordnare import anordnare
 from frontend.pages.karta import map_page
 
-#pages = {"home": home_page, "dashboard": dashboard_page, "data": data_page}
-pages = {"Home": home_page, "Educations": utbildningsomrade, "statistikansokningar": ansökningar, "Overview": gender_age, "karta": map_page}
+pages = {"Home": home_page, "Anordnare":anordnare, "Kursansokningar": ansökningar, "Utbildningsomraden": utbildningsomrade, "Demografi": gender_age, "karta": map_page}
 
 
 if __name__ == "__main__":
